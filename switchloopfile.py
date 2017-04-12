b7e4976928bb15a0ce2bf7fdc32dd65395811e2b
davidbombal <davidbombal@Davids-MacBook-Pro-3.local>
2017-04-12 20:20:58 +0100

    YouTube Video 8
    YouTube Video 8

---
+++ switchloopfile.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import getpass
+import sys
+import telnetlib
+
+user = raw_input("Enter your username: ")
+password = getpass.getpass()
+
+f = open ('myswitches')
+
+for line in f:
+    print "Configuring Switch " + (line)
+    HOST = line
+    tn = telnetlib.Telnet(HOST)
+
+    tn.read_until("Username: ")
+    tn.write(user + "\n")
+    if password:
+        tn.read_until("Password: ")
+        tn.write(password + "\n")
+
+    tn.write("conf t\n")
+
+    for n in range (2,11):
+        tn.write("vlan " + str(n) + "\n")
+        tn.write("name Python_VLAN_" + str(n) + "\n")
+
+    tn.write("end\n")
+    tn.write("exit\n")
+
+    print tn.read_all()
+
+
