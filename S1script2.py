f51210693ace495e508a160e9b6bff1e36ea2d72
davidbombal <davidbombal@Davids-MacBook-Pro-3.local>
2017-04-12 09:30:02 +0100

    YouTube Video Part 4 script uploaded
    YouTube Video Part 4 script uploaded

---
+++ S1script2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import getpass
+import sys
+import telnetlib
+
+HOST = "192.168.122.72"
+user = raw_input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("Username: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+
+tn.write("conf t\n")
+
+for n in range (2,101):
+    tn.write("vlan " + str(n) + "\n")
+    tn.write("name Python_VLAN_" + str(n) + "\n")
+
+tn.write("end\n")
+tn.write("exit\n")
+
+print tn.read_all()
