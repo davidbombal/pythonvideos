e6266767549e58bf661e0110e1b5c389342eed0a
davidbombal <davidbombal@Davids-MacBook-Pro-3.local>
2017-04-12 08:52:03 +0100

    Minor Edits
    Minor Script edits

--- S1script3.py
+++ S1script3.py
@@ -10,24 +10,24 @@ password = getpass.getpass()
 
 for n in range (72,77):
     print "Telnet to host" + str(n)
-	  HOST = "192.168.122." + str(n)
-	  tn = telnetlib.Telnet(HOST)
+    HOST = "192.168.122." + str(n)
+    tn = telnetlib.Telnet(HOST)
 
-	  tn.read_until("Username: ")
-	  tn.write(user + "\n")
-	  if password:
-	      tn.read_until("Password: ")
-	      tn.write(password + "\n")
+    tn.read_until("Username: ")
+    tn.write(user + "\n")
+    if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
 
 
-  	tn.write("conf t\n")
+    tn.write("conf t\n")
 
-	  for n in range (2,21):
-		    tn.write("vlan " + str(n) + "\n")
-		    tn.write("name Python_VLAN_" + str(n) + "\n")
+    for n in range (2,21):
+        tn.write("vlan " + str(n) + "\n")
+        tn.write("name Python_VLAN_" + str(n) + "\n")
 
-  	tn.write("end\n")
-	  tn.write("exit\n")
+    tn.write("end\n")
+    tn.write("exit\n")
 
-	  print tn.read_all()
+    print tn.read_all()
 
