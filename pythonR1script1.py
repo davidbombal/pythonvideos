cb75d57039ee947852fe4099f66b273c10d64078
davidbombal <davidbombal@Davids-MacBook-Pro-3.local>
2017-04-12 09:15:24 +0100

    YouTube Video Part 1
    Router configuration using Python

---
+++ pythonR1script1.py
@@ -0,0 +1,30 @@
+import getpass
+import sys
+import telnetlib
+
+HOST = "192.168.122.71"
+user = raw_input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until("Username: ")
+tn.write(user + "\n")
+if password:
+    tn.read_until("Password: ")
+    tn.write(password + "\n")
+
+
+tn.write("enable\n")
+tn.write("cisco\n")
+tn.write("conf t\n")
+tn.write("int loop 0\n")
+tn.write("ip address 1.1.1.1 255.255.255.255\n")
+tn.write("int loop 1\n")
+tn.write("ip address 2.2.2.2 255.255.255.255\n")
+tn.write("router ospf 1\n")
+tn.write("network 0.0.0.0 255.255.255.255 area 0\n")
+tn.write("end\n")
+tn.write("exit\n")
+
+print tn.read_all()
