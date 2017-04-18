003a9a6fe8f7de5543a902c8eb88a4f78a81f143
davidbombal <davidbombal@Davids-MacBook-Pro-3.local>
2017-04-18 16:41:43 +0100

    YouTube Video 13
    YouTube Video 13

---
+++ netmiko2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+
+iosv_l2_s1 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.71',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+iosv_l2_s2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.72',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+iosv_l2_s3 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.73',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+
+all_devices = [iosv_l2_s1, iosv_l2_s2, iosv_l2_s3]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    for n in range (2,21):
+       print "Creating VLAN " + str(n)
+       config_commands = ['vlan ' + str(n), 'name Python_VLAN ' + str(n)]
+       output = net_connect.send_config_set(config_commands)
+       print output 
