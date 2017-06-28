f154a11443ed1c67b94402e4925e53a173d6ff2a
davidbombal <davidbombal@Davids-MacBook-Pro-3.local>
2017-06-28 08:51:09 +0100

    YouTube Video 16

---
+++ netmiko5.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+from netmiko import ConnectHandler
+
+iosv_l2_s1 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.71',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+iosv_l2_s2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.72',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+iosv_l2_s3 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.73',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+iosv_l2_s4 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.74',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+iosv_l2_s5 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.75',
+    'username': 'david',
+    'password': 'cisco',
+}
+
+
+with open('iosv_l2_cisco_design') as f:
+    lines = f.read().splitlines()
+print lines
+
+
+all_devices = [iosv_l2_s5, iosv_l2_s4, iosv_l2_s3]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print output 
+
+
+with open('iosv_l2_core') as f:
+    lines = f.read().splitlines()
+print lines
+
+
+all_devices = [iosv_l2_s2, iosv_l2_s1]
+
+for devices in all_devices:
+    net_connect = ConnectHandler(**devices)
+    output = net_connect.send_config_set(lines)
+    print output 
